b7c578fd2907746394f720e58dac7e86d769cdf6
Rick <66313067+MandoRick@users.noreply.github.com>
2021-09-13 01:30:52 +0100

    Update FadeLeds_Pico.ino

--- FadeLeds_Pico.ino
+++ FadeLeds_Pico.ino
@@ -1,3 +1,18 @@
+/* -------------------
+   created by JediRick
+   -------------------
+   boolean (8 bit) true/false
+   byte (8 bit) - unsigned number from 0-255
+   char (8 bit) - signed number from -128 to 127
+   unsigned char (8 bit) - same as byte
+   word (16 bit) - unsigned number from 0-65535
+   unsigned int (16 bit)- the same as word
+   int (16 bit) - signed number from -32768 to 32767
+   unsigned long (32 bit) - unsigned number from 0-4,294,967,295  Usually for millis
+   long (32 bit) - signed number from -2,147,483,648 to 2,147,483,647
+   float (32 bit) - signed number from -3.4028235E38 to 3.4028235E38  Floating point not native so avoid if you can
+*/
+
 #define ledRed 1
 #define ledGreen 2
 #define ledBlue 3
