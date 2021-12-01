982b539205a7ace759b005398669239b7634fe8c
Rick <66313067+MandoRick@users.noreply.github.com>
2021-12-01 01:47:59 +0000

    Add files via upload

---
+++ ricksTimer/ricksTimer.ino
@@ -0,0 +1,43 @@
+/*    
+ *    Timer by JediRick 
+ *     
+ *    rasPi Pico:                     1334
+ *    Arduino Nano RP2040 Connect:    1408
+ *    Arduino Due:                    4464
+ *    Arduino Nano Every:             6854
+ *    Arduino Uno:                    6904
+ *    pro Micro:                      10560
+ *    elegoo Mega 2650:               11224
+ *    
+*/
+
+#define ledPin LED_BUILTIN
+word delayBetweenTimings = 1000;
+
+void setup() {
+  delay(1000);
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  delay(delayBetweenTimings);
+  runTimer();
+}
+
+void runTimer() {
+  uint32_t startTimer;
+  uint32_t endTimer;
+  uint32_t elapsed;
+  Serial.println("----------------------------------");
+  Serial.println("Running Timer");
+  startTimer = micros();
+  for (int i = 0; i < 1000; i++) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, LOW);
+  }
+  endTimer = micros();
+  elapsed = endTimer - startTimer;
+  Serial.println("Result in micros: " + (String)elapsed);
+  Serial.println("----------------------------------");
+}
