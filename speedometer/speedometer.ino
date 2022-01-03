9a57fb44dd5796b79bed3c32320f0aef7c0e543e
Rick <66313067+MandoRick@users.noreply.github.com>
2022-01-03 19:56:07 +0000

    Update speedometer.ino

--- speedometer/speedometer.ino
+++ speedometer/speedometer.ino
@@ -43,9 +43,10 @@
 #define wheelDiam 28
 #define sensorPin1 32
 #define sensorPin2 35
-#define tempSensor 36
+#define tempSensor 34
+#define pi 3.14159
 
-#define debug 0     // 1 ON 0 OFF
+#define debug 1     // 1 ON 0 OFF
 #if debug == 1
 #define debugln(x) Serial.println(x)
 #define debug(x) Serial.print(x)
@@ -56,22 +57,34 @@
 
 #include <SPI.h>
 #include <TFT_eSPI.h>
+#include "Free_Fonts.h"
 TFT_eSPI tft = TFT_eSPI();
 
 bool sensorState1 = true;
 bool sensorState2 = true;
 bool flipFlop = false;
+bool wheelFlop = false;
 bool udpateDisplay = true;
 
 uint8_t kilosPerHour = 0;
 unsigned long revsPerMin = 0;
-unsigned long finalSpeedKph = 0;
+float finalSpeedKph = 0;
 uint32_t rotationCount;
+float distanceTraveled;
 
 unsigned long currentMillis;
 unsigned long previousMillis = 0;
 unsigned long rotationTime;
 int16_t currentTemp;
+
+//------- duration timer stuff --------
+const unsigned long onTime1 = 100;
+const unsigned long offTime1 = 400;
+unsigned long previousMillis1 = 0;
+unsigned long currentMillis1;
+unsigned long interval1 = onTime1;
+boolean triggerState1 = true;
+//------- end duration timer stuff --------
 
 void setup() {
   delay(2000);
@@ -86,32 +99,36 @@ void setup() {
 
 void loop() {
   calculateSpeed();
-  checkTemp();
   drawDisplay();
+  drawDuration();
 }
 
 void setupDisplay() {
   tft.init();
   tft.setRotation(1);
+  tft.setFreeFont(FMB12);
   tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_GREEN);
-  tft.setTextSize(2);
-  tft.drawLine(0, 200, 320, 200, TFT_GREEN);
-  tft.setCursor(20, 25);
+  tft.setTextColor(TFT_RED);
+  tft.setCursor(20, 45);
   tft.println("KPH");
-  tft.setCursor(20, 100);
+  tft.setCursor(20, 85);
   tft.println("RPM");
-  tft.setCursor(20, 150);
+  tft.setCursor(20, 125);
   tft.println("TEMP");
+  tft.setCursor(20, 165);
+  tft.println("DIST");
+  tft.drawLine(0, 185, 320, 185, TFT_SKYBLUE);
+  tft.setCursor(20, 220);
+  tft.println("DUR");
 }
 
 void calculateSpeed() {
   if (digitalRead(sensorPin1) == 0) {
-    debugln("Sensor 1 LOW");
+    //debugln("Sensor 1 LOW");
     sensorState1 = false;
   }
   if (digitalRead(sensorPin2) == 0) {
-    debugln("Sensor 2 LOW");
+    //debugln("Sensor 2 LOW");
     sensorState2 = false;
   }
   if (sensorState1 == false && sensorState2 == false) {
@@ -119,43 +136,82 @@ void calculateSpeed() {
     sensorState2 = true;
     flipFlop = !flipFlop;
     if (flipFlop == true) {
+      checkTemp();
       currentMillis = millis();
       rotationTime = currentMillis - previousMillis;
       revsPerMin = 60000 / rotationTime;
       previousMillis = currentMillis;
       rotationCount += 1;
       finalSpeedKph = wheelDiam * revsPerMin * 0.001885;
+      distanceTraveled = ((wheelDiam * pi) * rotationCount) * 0.00001;
       udpateDisplay = true;
       debugln("Sensor triggered times: " + (String)rotationCount);
       debugln("Rotation time  millis: " + (String)rotationTime);
       debugln("Rotation RPM: " + (String)revsPerMin);
       debugln("Speed KPH: " + (String)finalSpeedKph);
+      debugln("Distance: " + (String)distanceTraveled);
     }
   }
 }
 
 void checkTemp() {
-  uint16_t tempValue = analogRead(tempSensor);
-  float pinVoltage = (tempValue / 4096) * 5;
-  float tempCelcius = (pinVoltage - 0.5) * 100;
-  currentTemp = tempCelcius;
-  debugln("temp C: " +(String)tempCelcius);
+  float tempPinVoltage = analogRead(tempSensor);
+  //debugln("Temp Sensor Value " + (String)tempPinVoltage);
+  tempPinVoltage = tempPinVoltage * 0.00122100;
+  currentTemp = (tempPinVoltage - 0.5) * 100.0;
+  debugln("temp C: " + (String)currentTemp);
 }
 
 void drawDisplay() {
   if (udpateDisplay == true) {
-    tft.setTextSize(7);
-    tft.fillRect(100, 0, 200, 170, TFT_BLACK);
+    tft.setFreeFont(FSB24);
+    tft.fillRect(95, 0, 210, 183, TFT_BLACK);
     tft.setTextColor(TFT_GREEN);
-    tft.setCursor(100, 25);
+    tft.setCursor(100, 50);
     tft.println(finalSpeedKph);
-    tft.setTextSize(4);
-    tft.setCursor(100, 100);
+    tft.setFreeFont(FSB18);
+    tft.setCursor(100, 90);
     tft.println(revsPerMin);
-    tft.setTextSize(3);
-    tft.setCursor(100, 150);
+    tft.setCursor(100, 130);
     tft.print(currentTemp);
-    tft.println("`C");
+    tft.println(" 'C");
+    tft.setCursor(100, 170);
+    tft.print(distanceTraveled);
+    tft.println(" km");
+    drawWheel();
     udpateDisplay = false;
+  }
+}
+
+void drawWheel() {
+  tft.fillCircle(290, 215 , 20, TFT_BLACK);
+  if (wheelFlop == true) {
+    tft.drawLine(280, 205, 300, 225, TFT_YELLOW);
+    tft.drawLine(300, 205, 280, 225, TFT_YELLOW);
+    wheelFlop = false;
+  } else {
+    tft.drawLine(290, 200, 290, 230, TFT_YELLOW);
+    tft.drawLine(275, 215, 305, 215, TFT_YELLOW);
+    wheelFlop = true;
+  }
+  tft.drawCircle(290, 215 , 15, TFT_ORANGE);
+}
+
+void drawDuration() {
+  currentMillis1 = millis();
+  long timeDuration = currentMillis1 / 1000;
+  if ((unsigned long)(currentMillis1 - previousMillis1) >= interval1) {
+    if (triggerState1) {
+      interval1 = offTime1;
+      tft.fillRect(95, 200, 170, 240, TFT_BLACK);
+      tft.setTextColor(TFT_YELLOW);
+      tft.setCursor(100, 225);
+      tft.println(timeDuration);
+    } else {
+      interval1 = onTime1;
+      //off time
+    }
+    triggerState1 = !(triggerState1);
+    previousMillis1 = currentMillis1;
   }
 }
